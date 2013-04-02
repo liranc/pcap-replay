de169289d529b94270401509570257e63dd0e005
liranc <liran.chen@gmail.com>
2013-04-02 21:07:03 +0300

    added mac resolving support to networkutils (not being used)

--- networkutils.c
+++ networkutils.c
@@ -2,17 +2,18 @@
 #include "networkutils.h"
 
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include <net/if.h>
 #include <linux/ip.h>
-#include <stdlib.h>
 #include <linux/if_packet.h>
 #include <linux/if_ether.h>
 #include <netinet/in.h>
 #include <sys/ioctl.h>
 #include <arpa/inet.h>
-#include <string.h>
-
+#include <unistd.h>
 
 int init_socket(const char * interface_name, int *sockfd, struct sockaddr_ll *addr) {
 
@@ -55,4 +56,100 @@ int is_supported_packet(struct ethhdr *packet){
 		return 0;
 
 	return 1;
+}
+
+int parse_mac_column(char *line, const char *lookup_ip, char *mac_result){
+
+	int column_num = 0;
+	char *column;
+
+	column = strtok(line, " ");
+	while(column){
+		++column_num;
+
+		switch(column_num){
+		case 1:
+			if(strcmp(lookup_ip, column) != 0)
+				return 0;
+			break;
+		case 3:
+			if(strcmp("0x0", column) == 0)
+				return 0;
+			break;
+		case 4:
+			strcpy(mac_result, column);
+			return 1;
+		}
+
+		column = strtok(NULL, " ");
+	}
+
+	return 0;
+}
+
+int resolve_mac_from_arp_table(const char *lookup_ip, char *mac){
+	FILE *arp_file = fopen("/proc/net/arp", "r");
+	if(arp_file == NULL){
+		perror("failed to open arp table file");
+		return 0;
+	}
+
+	int found = 0;
+	int line_num = 0;
+	char line_buff[2000] = {0};
+	while(fgets(line_buff, sizeof(line_buff), arp_file) != NULL){
+		++line_num;
+
+		if(line_num == 1)
+			continue;
+
+		if(parse_mac_column(line_buff, lookup_ip, mac)){
+			found = 1;
+			break;
+		}
+	}
+
+	fclose(arp_file);
+
+	return found;
+}
+
+int resolve_mac(const char *ip, unsigned char mac[ETH_ALEN]){
+
+	int dummy_sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if(dummy_sockfd == -1){
+		perror("failed to create mac resolving socket");
+		return 0;
+	}
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = 8872;
+	inet_aton(ip, &addr.sin_addr);
+	if(connect(dummy_sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1){
+		perror("failed to connect dummy socket");
+		return 0;
+	}
+
+	int retcode = 0;
+	clock_t start_time = clock();
+	do{
+		char mac_str[18] = {0};
+		if(resolve_mac_from_arp_table(ip, mac_str)){
+			printf("resolved MAC: %s for IP: %s\n", mac_str, ip);
+			retcode = 1;
+			break;
+		}
+		else{
+			printf("MAC address for %s wasn't found in ARP table. Resolving...\n", ip);
+			write(dummy_sockfd, NULL, 0);
+			sleep(1);
+		}
+	} while(((clock() - start_time) * 1000 / CLOCKS_PER_SEC) < 10000);
+
+	if(!retcode)
+		fprintf(stderr, "failed to resolve MAC address for IP: %s", ip);
+
+	close(dummy_sockfd);
+	return retcode;
 }
--- networkutils.h
+++ networkutils.h
@@ -8,5 +8,6 @@
 
 int init_socket(const char * interface_name, int *sockfd, struct sockaddr_ll *addr);
 int is_supported_packet(struct ethhdr *packet);
+int resolve_mac(const char *ip, unsigned char mac[ETH_ALEN]);
 
 #endif
